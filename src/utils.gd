bb75ca38f9757ab92f9219b58d384d6474409e65
bea <beatarosanus@gmail.com>
2026-08-12 15:23:17 -0300

    Delete src directory

--- src/utils.gd
+++
@@ -1,8 +0,0 @@
-class_name Utils 
-
-static func print_position(object: Node) -> void:
-	var pos: Vector2 = object.position
-	print_debug("%s.pos = (%s,%s)" % [object,pos.x,pos.y])
-
-static func get_physics_object(tree: SceneTree) -> Node:
-	return tree.current_scene.get_node("PhysicsObject")
